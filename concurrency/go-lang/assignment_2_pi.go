fb2601fc251da3e50ed27f941fe2cd43ca8ed464
liamgbs <liamgbs@github.com>
2017-11-14 15:55:28 +0000

    Added some comments to CAPS pi program

--- concurrency/go-lang/assignment_2_pi.go
+++ concurrency/go-lang/assignment_2_pi.go
@@ -19,24 +19,31 @@ func worker(result chan float64, darts_thrown int) {
       hitcount++
     }
   }
-  var partial_pi float64 = 4.0 * float64(hitcount) / float64(darts_thrown)
+  partial_pi := 4.0 * float64(hitcount) / float64(darts_thrown)
+  // send partial_pi to the channel (we dont care about the order they arrive in
+  // since theyre all sum reduced anyway).
   result <- partial_pi
 }
 
 func main() {
   threads := 4
   darts_thrown := 2000000
+  // create a buffered channel where the buffer amount is equal to threads
+  // ie. we can have thread number of values waiting to be read from the channel
   result := make(chan float64, threads)
   var pi float64
 
+  // Spawn thread number of worker threads all running concurrently
   for i := 0; i < threads; i++ {
     go worker(result, darts_thrown / threads)
   }
 
+  // take all the buffered values from the channel and add them together
   for j := 0; j < threads; j++ {
     pi = pi + <- result
   }
 
+  // divide the sum of all thread results by the number of threads to get pi
   pi = pi / float64(threads)
 
   fmt.Println("Estimated pi:", pi)
